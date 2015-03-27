cad1ff936c5f11669c9c0a1943095abfd46134e0
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-27 22:34:44 +1100

    started adding templates to index.html

--- app/assets/javascripts/views/appView.js
+++ app/assets/javascripts/views/appView.js
@@ -3,7 +3,8 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
   el: '#main',
   render: function() {
-    var html = // template html
-    this.$el.html(html);
+    console.log('Rendering AppView...');
+    // var html = // template html
+    // this.$el.html(html);
   }
 });
