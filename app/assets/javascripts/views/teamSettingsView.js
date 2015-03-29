8cf1976d15bc4b37568085d2bf96951c1e15cf37
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-29 21:05:59 +1100

    edit form events wired up and ready for correct data posting method

--- app/assets/javascripts/views/teamSettingsView.js
+++ app/assets/javascripts/views/teamSettingsView.js
@@ -2,9 +2,21 @@ var app = app || {};
 
 app.TeamSettingsView = Backbone.View.extend({
   el: '#main',
+  events: {
+    'click #team-submit': 'updateTeam'
+  },
   render: function() {
     this.$el.empty();
     var html = $('#teamSettingsView-template').html();
     this.$el.html(html);
+    // Fetch the current user's team here.
+    // Then populate the fields with the right information.
+  },
+  updateTeam: function() {
+    var teamName = $('#team-name').val();
+    var teamEmblem = $('#team-emblem').val();
+
+    // Updating here relies on passing in the right model to this view...I think?
+    debugger;
   }
 });
--- app/assets/javascripts/views/userSettingsView.js
+++ app/assets/javascripts/views/userSettingsView.js
@@ -2,9 +2,17 @@ var app = app || {};
 
 app.UserSettingsView = Backbone.View.extend({
   el: '#main',
+  events: {
+    'click #user-submit': 'updateUser'
+  }
   render: function() {
     this.$el.empty();
     var html = $('#userSettingsView-template').html();
     this.$el.html(html);
+  },
+  updateUser: function() {
+    var profileURL = $('#profile-image').val();
+
+    // Updating here relies on passing in the right model to this view...I think?
   }
 });
