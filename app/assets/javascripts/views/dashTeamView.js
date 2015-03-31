bedd39a04a9be9fda18d595ba58e5ba95ead500a
Tom Valorsa <tomvalorsa@gmail.com>
2015-03-31 23:02:58 +1100

    formations now present on the pitch-graphic

--- app/assets/javascripts/views/dashTeamView.js
+++ app/assets/javascripts/views/dashTeamView.js
@@ -58,6 +58,30 @@ app.DashTeamView = Backbone.View.extend({
     var tactic = app.tactics.get(app.userTactic.id);
 
     tactic.set({'formation': formation});
+
+    // Need to get this to work in the render function as well, so that when a user goes to their dashboard it shows the current team.
+    $('#pitch-graphic').attr('src', function() {
+        switch (tactic.toJSON().formation) {
+          case "4-4-2":
+            return "http://i.imgur.com/cq6W5KY.png"
+            break;
+          case "4-3-3":
+            return "http://i.imgur.com/sPVh0xx.png"
+            break;
+          case "4-5-1":
+            return "http://i.imgur.com/ua5O1on.png"
+            break;
+          case "4-1-2-1-2":
+            return "http://i.imgur.com/RQa21gm.png"
+            break;
+          case "4-1-4-1":
+            return "http://i.imgur.com/s2ToTgJ.png"
+            break;
+          default:
+            return "http://i.imgur.com/cq6W5KY.png"
+        }
+      });
+
     tactic.save();
   }
 });
