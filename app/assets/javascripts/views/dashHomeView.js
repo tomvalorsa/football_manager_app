65d1c9dc778d5cd7b88ece83421b4109aca1f2f5
Tom Valorsa <tomvalorsa@gmail.com>
2015-05-02 15:59:14 +1000

    refactored dashHomeView using multiple functions

--- app/assets/javascripts/views/dashHomeView.js
+++ app/assets/javascripts/views/dashHomeView.js
@@ -5,43 +5,20 @@ app.DashHomeView = Backbone.View.extend({
   render: function() {
     this.$el.empty();
 
-    var dashHomeViewTemplate = $('#dashHomeView-template').html();
-    var dashHomeViewHTML = _.template(dashHomeViewTemplate);
+    var dashHomeViewTemplate = $('#dashHomeView-template').html(),
+        dashHomeViewHTML = _.template(dashHomeViewTemplate);
     this.$el.html(dashHomeViewHTML(app.userTeam));
 
+    $('#home-balance').html(accounting.formatMoney($('#home-balance').html()));
+    $('#home-value').html(accounting.formatMoney($('#home-value').html()));
+
+    this.printMatchInfo();
+    this.printStarRating();
+    this.fillFormBar();
+    this.displayWLDChart();
+  },
+  displayWLDChart: function() {
     var dataPoints = [];
-
-    $.get('/user-match-data', function(result) {
-      // 'result' here is an array of match objects that belong to the user's team,
-      // i.e. matches they have played.
-
-      // Gets the last 5 results from the user's team's match array.
-      var recentMatches = result.slice(Math.max(result.length - 5, 0));
-
-      // Use recentMatches here instead of a direct slice to make the check for the last array item dynamic.
-      _.each(recentMatches, function(match, i) {
-        if(match.home_team_id === app.userTeam.id) {
-          if(match === _.last(recentMatches)) {
-            $('#recent-results').append(match.home_result);
-          } else {
-            $('#recent-results').append(match.home_result + ' - ');
-          }
-        } else if(match.away_team_id === app.userTeam.id) {
-          if(match === _.last(recentMatches)) {
-            $('#recent-results').append(match.away_result);
-          } else {
-            $('#recent-results').append(match.away_result + ' - ');
-          }
-        }
-      });
-
-      // Throw up the all matches with teams and scores to the dash.
-      _.each(result, function(match) {
-        var matchListView = new app.MatchListView({model: match});
-        matchListView.render();
-      });
-
-    });
 
     $.get('/current-user-team-stats', function(result) {
       $('#team-emblem').attr('src', result.emblem);
@@ -49,9 +26,7 @@ app.DashHomeView = Backbone.View.extend({
       dataPoints.push(result.win);
       dataPoints.push(result.draw);
       dataPoints.push(result.loss);
-
     }).done(function() {
-
       var wldData = [
         {
           value: dataPoints[2],
@@ -73,54 +48,77 @@ app.DashHomeView = Backbone.View.extend({
         }
       ];
 
-      var $wldChart = $('#wldChart');
-      var wldCtx = $wldChart.get(0).getContext('2d');
-      var wldLineChart = new Chart(wldCtx).Doughnut(wldData);
+      var $wldChart = $('#wldChart'),
+          wldCtx = $wldChart.get(0).getContext('2d'),
+          wldLineChart = new Chart(wldCtx).Doughnut(wldData);
 
       $('#wld-chart').append(wldLineChart);
+    });
+  },
+  printMatchInfo: function() {
+    $.get('/user-match-data', function(result) {
+      // Gets the last 5 results from the user's team's match array.
+      var recentMatches = result.slice(Math.max(result.length - 5, 0));
 
-      $('#home-balance').html(accounting.formatMoney($('#home-balance').html()));
-      $('#home-value').html(accounting.formatMoney($('#home-value').html()));
-
-      // Star Ratings:
-      var rating = app.userTeam.overall_rating;
-
-      var customFloor = function(value, roundTo) {
-        return Math.floor(value / roundTo) * roundTo;
-      }
-
-      // Round rating down to the nearest multiple of 10.
-      var rating = customFloor(rating, 10);
-
-      $('#star-rating').html(function() {
-        var result = '';
-        var star = '<i class="fa fa-star"></i>';
-        var halfStar = '<i class="fa fa-star-half-o"></i>';
-        var emptyStar = '<i class="fa fa-star-o"></i>';
-
-        while (rating > 0) {
-          if (rating - 20 >= 0) {
-            rating = rating - 20;
-            result += star + ' ';
-          } else if (rating - 10 >= 0) {
-            rating = rating - 10;
-            result += halfStar;
+      _.each(recentMatches, function(match, i) {
+        if(match.home_team_id === app.userTeam.id) {
+          if(match === _.last(recentMatches)) {
+            $('#recent-results').append(match.home_result);
+          } else {
+            $('#recent-results').append(match.home_result + ' - ');
+          }
+        } else if(match.away_team_id === app.userTeam.id) {
+          if(match === _.last(recentMatches)) {
+            $('#recent-results').append(match.away_result);
+          } else {
+            $('#recent-results').append(match.away_result + ' - ');
           }
         }
-
-        // Pad the ratings with empty stars.
-        while (result.split('</i>').length != 6) {
-          result += ' ' + emptyStar;
-        }
-
-        return result;
       });
 
-      // Finds the current user's team's form rating and sets it as a percentage for the green fill div.
-      $('#form-bar-fill').attr('style', function() {
-        var form = app.userTeam.form_rating
-        return "background-color: #66bb6a; border-radius: 15px; height: 100%; padding: 0; width: " + form + "%;"
-      })
+      // Render list of matches played.
+      _.each(result, function(match) {
+        var matchListView = new app.MatchListView({model: match});
+        matchListView.render();
+      });
+    });
+  },
+  printStarRating: function() {
+    var customFloor = function(value, roundTo) {
+      return Math.floor(value / roundTo) * roundTo;
+    }
+
+    // Round rating down to the nearest multiple of 10.
+    var rating = customFloor(app.userTeam.overall_rating, 10);
+
+    $('#star-rating').html(function() {
+      var result = '',
+          star = '<i class="fa fa-star"></i>',
+          halfStar = '<i class="fa fa-star-half-o"></i>',
+          emptyStar = '<i class="fa fa-star-o"></i>';
+
+      while (rating > 0) {
+        if (rating - 20 >= 0) {
+          rating = rating - 20;
+          result += star + ' ';
+        } else if (rating - 10 >= 0) {
+          rating = rating - 10;
+          result += halfStar;
+        }
+      }
+
+      // Pad the ratings with empty stars.
+      while (result.split('</i>').length != 6) {
+        result += ' ' + emptyStar;
+      }
+
+      return result;
+    });
+  },
+  fillFormBar: function() {
+    $('#form-bar-fill').attr('style', function() {
+      var form = app.userTeam.form_rating;
+      return "background-color: #66bb6a; border-radius: 15px; height: 100%; padding: 0; width: " + form + "%;"
     });
   }
 });
